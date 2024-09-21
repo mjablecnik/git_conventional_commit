0a6d1190b02e8ce0baa2aac9f5573ae4d424535d
Martin Jablečník <martin.jablecnik@email.cz>
2024-09-21 23:41:56 +0200

    chore: init project

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:git_conventional_commit/app_info.dart';
+
+import 'args_parser.dart';
+
+/*
+
+Example of usage:
+dart lib/main.dart -V --name Martin -t dart,python,java
+
+ */
+void main(List<String> args) {
+  Arguments.parse(args).then((args) async {
+    if (args.showHelp) {
+      showHelp();
+    } else if (args.showVersion) {
+      print("Version: ${await AppInfo().version}");
+    } else {
+      print("Verbose: ${args.isVerbose}");
+      print("Name: ${args.name}");
+      print("Tags: ${args.tags}");
+    }
+  }, onError: (error) {
+    if (error is FormatException) {
+      print(error.message);
+      showHelp();
+      return;
+    }
+    print(error);
+  });
+}
+
+
+showHelp() {
+  print("\nUsage:");
+  print(Arguments.usage);
+}
