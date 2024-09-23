918acd26d22a3afd2f2d182789304739bb472bb7
Martin Jablečník <martin.jablecnik@email.cz>
2024-09-23 13:58:57 +0200

    style: remove develop helper prints

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,8 @@ import 'package:git_conventional_commit/git_command_builder.dart';
 import 'args_parser.dart';
 
 /*
-
 Example of usage:
 dart lib/main.dart -t feat -m "Add new feature" -b -s '#1234'
-
-gc -m 'Add something'
-
  */
 void main(List<String> args) {
   Arguments.parse(args).then((args) async {
@@ -20,12 +16,6 @@ void main(List<String> args) {
     } else if (args.showVersion) {
       print("Version: ${await AppInfo().version}");
     } else {
-      print("Verbose: ${args.isVerbose}");
-      print("Type: ${args.commitType}");
-      print("Message: ${args.commitMessage}");
-      print("Scope: ${args.commitScope}");
-      print("Breaking: ${args.isBreakingChange}");
-
       final List<String> gitArgs;
 
       if (args.amend) {
